9067b2bb1edefd1049f2f28fa4a12f0b568cc37e
Alok_Gusain06 <129003122+alokgusain06@users.noreply.github.com>
2025-04-13 10:30:53 +0530

    Update PostJob.jsx

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -42,7 +42,7 @@ const PostJob = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post(`${JOB_API_END_POINT}/post`, input,{
+            const res = await axios.post(`https://careenow-1.onrender.com/api/v1/job/postjob, input,{
                 headers:{
                     'Content-Type':'application/json'
                 },
