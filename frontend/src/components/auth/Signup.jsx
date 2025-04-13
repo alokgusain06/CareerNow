c96a5fc84cc465a7ed4fd53cf0137537d66f5224
Alok_Gusain06 <129003122+alokgusain06@users.noreply.github.com>
2025-04-13 10:13:25 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -48,7 +48,7 @@ const Signup = () => {
 
     try {
       dispatch(setLoading(true));
-      const res = await axios.post(`https://careenow-1.onrender.com/api/v1/user/register`, formData, {
+      const res = await axios.post("https://careenow-1.onrender.com/api/v1/user/register", formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
         withCredentials: true,
       });
